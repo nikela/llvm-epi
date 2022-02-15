41a282c3e859d971d7dd771b0708225752b61e8b
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2022-02-15 11:20:35 +0000

    Update test

--- clang/test/Driver/riscv-default-features.c
+++ clang/test/Driver/riscv-default-features.c
@@ -1,8 +1,8 @@
 // RUN: %clang -target riscv32-unknown-elf -S -emit-llvm %s -o - | FileCheck %s -check-prefix=RV32
 // RUN: %clang -target riscv64-unknown-elf -S -emit-llvm %s -o - | FileCheck %s -check-prefix=RV64
 
-// RV32: "target-features"="+a,+c,+m"
-// RV64: "target-features"="+64bit,+a,+c,+m"
+// RV32: "target-features"="+a,+c,+m,-relax,-save-restore"
+// RV64: "target-features"="+64bit,+a,+c,+m,-relax,-save-restore"
 
 // Dummy function
 int foo(){
