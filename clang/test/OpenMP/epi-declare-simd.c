267d0ff6996fdec61540ec33e1a64bca2ed9cb14
Lorenzo Albano <lorenzo.albano@bsc.es>
2022-07-07 09:43:50 +0000

    Correctly create the VFABI mangled attributes name.

--- clang/test/OpenMP/epi-declare-simd.c
+++ clang/test/OpenMP/epi-declare-simd.c
@@ -2,15 +2,17 @@
 // RUN: %clang_cc1 -triple riscv64-linux-gnu -target-feature +zepi -fopenmp-simd -emit-llvm -O2 -o - %s -verify
 
 #pragma omp declare simd
-#pragma omp declare simd simdlen(1) // expected-warning {{The value specified in simdlen multiplied by the size of the data type must be included in the [64,512] range.}}
-#pragma omp declare simd simdlen(5) // expected-warning {{The value specified in simdlen must be a power of 2.}}
+#pragma omp declare simd simdlen(1) // expected-warning {{omp_max_simdlen is required in EPI.}}
+#pragma omp declare simd simdlen(omp_max_simdlen)
+#pragma omp declare simd simdlen(omp_max_simdlen: 1)
+#pragma omp declare simd simdlen(omp_max_simdlen: 5) // expected-warning {{The value specified in simdlen must be a power of 2.}}
 float fsquare(float a) {
   return a * a;
 }
 
-// RISCV64: "_ZGVEMk16v_fsquare" "_ZGVEMk2v_fsquare" "_ZGVEMk4v_fsquare" "_ZGVEMk8v_fsquare" "_ZGVENk16v_fsquare" "_ZGVENk2v_fsquare" "_ZGVENk4v_fsquare" "_ZGVENk8v_fsquare"
+// RISCV64: "_ZGVEMk1v_fsquare" "_ZGVEMk2v_fsquare" "_ZGVEMk4v_fsquare" "_ZGVEMk8v_fsquare" "_ZGVENk1v_fsquare" "_ZGVENk2v_fsquare" "_ZGVENk4v_fsquare" "_ZGVENk8v_fsquare"
 
-#pragma omp declare simd simdlen(1)
+#pragma omp declare simd simdlen(omp_max_simdlen: 1)
 double dsquare(double a) {
   return a * a;
 }
@@ -31,14 +33,14 @@ double bsquare(double a) {
 
 // RISCV64: "_ZGVEMk1v_bsquare" "_ZGVEMk2v_bsquare" "_ZGVEMk4v_bsquare" "_ZGVEMk8v_bsquare"
 
-#pragma omp declare simd simdlen(1) linear(a : 1)
+#pragma omp declare simd simdlen(omp_max_simdlen: 1) linear(a : 1)
 double lsquare(double *a) {
   return *a * *a;
 }
 
 // RISCV64: "_ZGVEMk1l8_lsquare" "_ZGVENk1l8_lsquare"
 
-#pragma omp declare simd simdlen(1) uniform(b)
+#pragma omp declare simd simdlen(omp_max_simdlen: 1) uniform(b)
 double usquare(double a, double b) {
   return a * b;
 }
