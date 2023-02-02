8afdff6c71f07583dd4ffebd6cb9ba97641cd0c3
ManuelJBrito <manuel.brito@tecnico.ulisboa.pt>
2023-02-02 10:11:53 +0000

    [Clang] Add builtin_nondeterministic_value
    Differential Revision: https://reviews.llvm.org/D142388

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -13573,6 +13573,8 @@ private:
   bool PrepareBuiltinElementwiseMathOneArgCall(CallExpr *TheCall);
   bool PrepareBuiltinReduceMathOneArgCall(CallExpr *TheCall);
 
+  bool SemaBuiltinNonDeterministicValue(CallExpr *TheCall);
+
   // Matrix builtin handling.
   ExprResult SemaBuiltinMatrixTranspose(CallExpr *TheCall,
                                         ExprResult CallResult);
